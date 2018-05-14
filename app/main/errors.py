5df44b89c4150b25ee6604da8a5aecdc4b055db9
Imma7 <immamugambi@gmail.com>
2018-05-14 17:41:05 +0300

    Passing in the status code we receive 404

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,5 +4,8 @@ from app import app #Importing flask application instance
 @app.errorhandler(404) #Decorator that passes in the error we receive
 def four_Ow_four(error): #View function that returns fourOwfour.html file
     '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html'),404 #Passing in the status code we receive 404
 
 
