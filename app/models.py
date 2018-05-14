80aa71b5a8e6cddf213f9cbb6d289070a4d18021
Imma7 <immamugambi@gmail.com>
2018-05-14 18:33:24 +0300

    Created Sources class

--- app/models.py
+++ app/models.py
@@ -1,2 +1,17 @@
 class Article:
+    '''
+    Article class to define article objects
+    '''
+
+    def __init__(self,id,name,author,title,description,url, urlToImage,publishedAt):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+
+class SOurces:
     
